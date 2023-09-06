eff3f747766835d5ade0a1ab8819d16dbfdccc1a
b2sigep <138058967+b2sigep@users.noreply.github.com>
2023-09-06 11:11:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-/home/appuser/venv/bin/python -m pip install --upgrade pip
+# /home/appuser/venv/bin/python -m pip install --upgrade pip
 
 import snowflake.connector
 
