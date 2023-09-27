67c1c5ecb89174e9c845457afc971697ee14b7f1
b2sigep <138058967+b2sigep@users.noreply.github.com>
2023-09-27 13:25:50 -0500

    Update streamlit_app.py
    updated Avocado Toast to text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.title('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
