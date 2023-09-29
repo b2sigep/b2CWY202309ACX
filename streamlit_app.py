fc31083489e620196ce3f35d43050ca42da4a77b
b2sigep <138058967+b2sigep@users.noreply.github.com>
2023-09-29 12:57:08 -0500

    Update streamlit_app.py
    #remome the just writes to the screen line from the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" "watermelon + kiwi")
+# delete this line streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
