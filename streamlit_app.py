90d94be26acf44e822ee80626b6995a29c91d37d
b2sigep <138058967+b2sigep@users.noreply.github.com>
2023-09-29 12:58:33 -0500

    Update streamlit_app.py
    added kiwi to fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" "watermelon + kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"  + "kiwi")
 # delete this line streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # take the json version of the response and normalize it 
